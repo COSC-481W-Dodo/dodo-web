ed2552e9460d7e1ff366457a794fa75714f73392
Brandon Jones <brandonijones@outlook.com>
2022-10-14 19:21:32 -0400

    feat: Implement email verification
    * feat: Implement email verification
    * Added basic form validation using dependencies Formik and Yup.
        * Checks if all fields have been entered.
        * Checks for valid email.
        * Checks if email is already in use after submission.
        * Checks if password has at least 8 characters. Still needs validation for other criteria.
        * Error messages appear if the form is not valid.
    * Improved styling for the registration form.
    * Updated the form and register user functions to include the username.
    * After successful submission, a verification email gets sent to the user. The verification email uses Firebase's default template.
    * After successful submission, a message appears telling the user to check their email for the verification link.
    * Since Formik automatically handles the onChange data, the onChange events were removed for all fields.
    * Added some text to the home page.
    
    Issue #1

--- src/Data/Repository/UserRepository.ts
+++ src/Data/Repository/UserRepository.ts
@@ -1,8 +1,15 @@
-import { db } from "../DataSource/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, updateProfile, sendEmailVerification } from "firebase/auth";
 import { auth } from "../DataSource/firebase";
 
-export async function registerUser(email: string, password: string) {
+export async function registerUser(email: string, username: string, password: string) {
     const result = await createUserWithEmailAndPassword(auth, email, password);
+    const user = result.user;
+
+    sendEmailVerification(user);
+    
+    updateProfile(user, {
+        displayName: username
+    })
+    console.log(auth.currentUser);
     return result;
 }
--- src/Presentation/Views/Home/Home.tsx
+++ src/Presentation/Views/Home/Home.tsx
@@ -3,7 +3,8 @@ function Home() {
 
     return (
         <div>
-            <h1>Home Page</h1>
+            <h1>Welcome to Dodo</h1>
+            <h3>A study app for...dodos...</h3>
         </div>
     );
 }
