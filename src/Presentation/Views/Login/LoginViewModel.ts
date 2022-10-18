3444645e5290d8ffe1194a7f752f0991d104bb4b
Jeremy Genovese <jgenove1@emich.edu>
2022-10-17 22:34:42 -0400

    feat: Implement login form validation
    feat: Implement login form validation
    
    * added a username entry box to the login page
    * added warning for blank email
    * added warning for invalid email
    * added warning for blank username
    * added warning for blank password
    * added warning for password that does not meet the requirements
    
    Issue #2

--- src/Presentation/Views/Login/LoginViewModel.ts
+++ src/Presentation/Views/Login/LoginViewModel.ts
@@ -1,12 +1,42 @@
 import { useState } from "react";
 import { LoginUserUseCase } from '../../../Domain/UseCase/User/LoginUser';
+import * as Yup from 'yup';
+
+interface NewUser {
+    email: string;
+    username: string;
+    password: string
+}
 
 
 export default function LoginViewModel() {
     const [loginEmail, setLoginEmail] = useState("");
     const [loginPassword, setLoginPassword] = useState("");
 
-    async function onClickLogin() {
+    // initial form values
+    const initialValues = {
+        email: "",
+        username: "",
+        password: "",
+    }
+
+    // Validation criteria for each field in the form
+    const validationSchema = Yup.object().shape({
+        email: Yup.string().email("Please enter a valid email address.").required("Email is required."),
+        username: Yup.string().max(30, "Too Long! Maximum 30 characters.").required("Username is required."),
+        password: Yup.string().min(8, 'Must be at least 8 characters long' ).required("Please provide a password.").matches(/^(?=.*[A-Z])(?=.*[()@#$%^&+=]).*$/, "Password must have at least one capital letter and one special character."),
+    });
+
+    async function onClickLogin(formData: NewUser) {
+        const loginEmail = formData.email;
+        const loginPassword = formData.password;
+
+        setLoginEmail(loginEmail);
+        console.log(formData);
+
+        setLoginPassword(loginPassword);
+        console.log(formData);
+
         try {
             //Logs the user in if they are registered
             const user = await LoginUserUseCase(loginEmail, loginPassword);
@@ -29,6 +59,8 @@ export default function LoginViewModel() {
     return {
         onClickLogin,
         onChangeEmail,
-        onChangePassword
+        onChangePassword,
+        initialValues,
+        validationSchema
     }
 }
