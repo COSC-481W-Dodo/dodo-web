e6158d753319c48c093297d00cbf7c008b13f774
michaela gerweck <mgerwec1@emich.edu>
2022-11-06 14:10:18 -0500

    feat: Upload Card Data to Firebase
    *Created CreateTag Usecase
    *Created CreatFlashcard Usecase
    *Created Repository to add tags to database
    *Created Repository to add flashcard information to database
    
    Issue #15

--- src/Data/Repository/TagRepository.ts
+++ src/Data/Repository/TagRepository.ts
@@ -1,1 +1,8 @@
-export {};
+import { doc, setDoc } from 'firebase/firestore';
+import { db } from '../DataSource/firebase';
+
+export async function createTag(tagName: string) {
+    const result = await setDoc(doc(db, "tags", tagName), { });
+
+    return result;
+}
--- src/Domain/UseCase/Tag/CreateTag.ts
+++ src/Domain/UseCase/Tag/CreateTag.ts
@@ -1,1 +1,5 @@
-export {};
+import { createTag } from '../../../Data/Repository/TagRepository';
+
+export async function CreateTagUseCase(tagName: string) {
+    return await createTag(tagName);
+}
