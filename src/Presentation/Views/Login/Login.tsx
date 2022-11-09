dccbaf021247b6338645074fae42f475ac543e97
Jeremy Genovese <jgenove1@emich.edu>
2022-11-09 11:17:56 -0500

    feat: Redirect user on successful login
    feat: Redirect user on successful login
    * add code to check the current auth state
    * add conditional rendering to the login page to redirect to '/' if the user is not 'null'
    
    Issue #16

--- src/Presentation/Views/Login/Login.tsx
+++ src/Presentation/Views/Login/Login.tsx
@@ -1,8 +1,15 @@
 import useViewModel from './LoginViewModel';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import React, { useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../../../Data/DataSource/firebase";
+import { Navigate } from "react-router-dom";
 
 function Login() {
-
+    const [user, setUser] = useState({});
+    React.useEffect(() => {
+        onAuthStateChanged(auth, (currentUser: any) => {setUser(currentUser);})
+    },[]);
     const {
         onClickLogin,
         onClickShowPassword,
@@ -15,42 +22,54 @@ function Login() {
     } = useViewModel();
 
     return (
-        <div>
-            <h1>Login Page</h1>
-            <h3>Login to use Dodo</h3>
+        <>
+            { auth.currentUser !== null ?
+                <Navigate to="/" />
+                :
+                <div>
+                    <h1>Login Page</h1>
+                    <h3>Login to use Dodo</h3>
 
-            <div>
-                { 
-                    <Formik
-                        initialValues={initialValues}
-                        onSubmit={onClickLogin}
-                        validationSchema={validationSchema}
-                    >
-                        <Form className='col-lg-4 offset-lg-4'>
-                            
-                            <div className="row justify-content-center">
-                                <label>Email Address</label>
-                                <Field name="email" type="email" className="form-control" placeholder="Email..." />
-                                <ErrorMessage name="email" render={message => <div className='text-danger'>{message}</div>} />
-                            </div>
-                            <br></br>
-                            <div className="row justify-content-center">
-                                <label>Password</label>
-                                <Field name="password" type={passwordType} className="form-control" placeholder="Password..."/>
-                                <div className="form-check mt-2">
-                                    <input className="form-check-input" type="checkbox" onClick={onClickShowPassword} />
-                                    <label className="form-check-label">Show password</label>
-                                </div>
-                                <ErrorMessage name="password" render={message => <div className='text-danger'>{message}</div>} />  
-                            </div>
-                            <br></br>
-                            <p>Need an account? <a href="/registration">Register</a> </p>
-                            <button type="submit" className="btn btn-primary">Login</button>
-                        </Form>
-                    </Formik>
-                }
-            </div>
-        </div>
+                    <div>
+                        {
+                            <Formik
+                                initialValues={initialValues}
+                                onSubmit={onClickLogin}
+                                validationSchema={validationSchema}
+                            >
+                                <Form className='col-lg-4 offset-lg-4'>
+
+                                    <div className="row justify-content-center">
+                                        <label>Email Address</label>
+                                        <Field name="email" type="email" className="form-control"
+                                               placeholder="Email..."/>
+                                        <ErrorMessage name="email"
+                                                      render={message => <div className='text-danger'>{message}</div>}/>
+                                    </div>
+                                    <br></br>
+                                    <div className="row justify-content-center">
+                                        <label>Password</label>
+                                        <Field name="password" type={passwordType} className="form-control"
+                                               placeholder="Password..."/>
+                                        <div className="form-check mt-2">
+                                            <input className="form-check-input" type="checkbox"
+                                                   onClick={onClickShowPassword}/>
+                                            <label className="form-check-label">Show password</label>
+                                        </div>
+                                        <ErrorMessage name="password"
+                                                      render={message => <div className='text-danger'>{message}</div>}/>
+                                    </div>
+                                    <br></br>
+                                    <p>Need an account? <a href="/registration">Register</a></p>
+                                    <button type="submit" className="btn btn-primary">Login</button>
+                                </Form>
+                            </Formik>
+                        }
+                    </div>
+                </div>
+
+            }
+            </>
     );
 }
 
