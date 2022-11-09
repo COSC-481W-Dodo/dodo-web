fa8a60c7b771de97a3d04e4f681e298748358594
Sam Keim <sblasko1@emich.edu>
2022-11-08 19:35:34 -0500

    feat: Dynamic navigation bar based on login state
    Navigation bar hides login and register buttons when user is logged in.
    Navigation bar hides edit user and view user buttons when user is not logged in.
    
    Issue #16

--- src/Presentation/Components/NavigationBar.tsx
+++ src/Presentation/Components/NavigationBar.tsx
@@ -1,12 +1,19 @@
+import React, {useState} from "react";
 import { Link, Outlet } from 'react-router-dom';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
+import { onAuthStateChanged, User } from 'firebase/auth';
+import { auth } from "../../Data/DataSource/firebase";
 
 import './navigation-bar.css'
 
 function NavigationBar() {
-
+    const [user, setUser] = useState({});
+    React.useEffect(() => {
+        onAuthStateChanged(auth, (currentUser: any) => {setUser(currentUser);})  
+      },[]);
+      
     return (
         <>
             <Navbar bg="dark" variant="dark">
@@ -14,11 +21,17 @@ function NavigationBar() {
                     <Navbar.Brand><Link to='/'>Dodo</Link></Navbar.Brand>
                     <Nav>
                         <Link to='/'>Home</Link>
+                        { auth.currentUser === null ?
+                        <>
                         <Link to='/login'>Login</Link>
                         <Link to='/registration'>Registration</Link>
+                        </>
+                        :
+                        <>
                         <Link to='/edit-account'>Edit Account</Link>
                         <Link to='/view-account'>View Account</Link>
-
+                        </>
+                        }
                     </Nav>
                 </Container>
             </Navbar>
