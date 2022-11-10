1d0d6422b756935f75c2248d6f0429dca21dc07d
Brandon Jones <brandonijones@outlook.com>
2022-11-09 21:34:30 -0500

    feat: Prepare for sprint 2 deployment
    * Included new download link for the android app
    * Cleaned some redundant code
    * Removed placeholder h1 tags for login and registration page for a cleaner look
    
    Issue #21

--- src/Presentation/Views/Home/Home.tsx
+++ src/Presentation/Views/Home/Home.tsx
@@ -7,7 +7,7 @@ function Home() {
             <h1 className="home-header mt-3">Welcome to Dodo</h1>
             <h3>A study app for...dodos...</h3>
             <br />
-            <h3><a href="https://www.dropbox.com/s/rl5qpafaubwuuy9/dodo_sprint1.apk?dl=0" target="_blank" rel="noreferrer">Download the App</a></h3>
+            <h3><a href="https://www.dropbox.com/s/7ihnx8x0hogbodz/app-debug.apk?dl=0" target="_blank" rel="noreferrer">Download the App</a></h3>
         </div>
     );
 }
