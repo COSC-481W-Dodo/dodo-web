54fb906665c8db00207e89b6b864f65be1e40066
Brandon Jones <brandonijones@outlook.com>
2022-11-16 19:46:48 -0500

    refactor: Start implementation of tag ids
    * A readable tag id is created based on the user input.
    * Still need to run a few more checks to make sure everything is working correctly
    
    Issue #29

--- src/Data/Repository/TagRepository.ts
+++ src/Data/Repository/TagRepository.ts
@@ -1,8 +1,12 @@
-import { doc, setDoc } from 'firebase/firestore';
+import { arrayUnion, doc, setDoc } from 'firebase/firestore';
+import { Tag } from '../../Common/interfaces';
 import { db } from '../DataSource/firebase';
 
-export async function createTag(tagName: string) {
-    const result = await setDoc(doc(db, "tags", tagName), { });
+export async function createTag(newTag: Tag, userId: string) {
+    const result = await setDoc(doc(db, "tags", newTag.id), { 
+        name: newTag.tagName,
+        authors: arrayUnion(newTag.id)
+    });
 
     return result;
 }
