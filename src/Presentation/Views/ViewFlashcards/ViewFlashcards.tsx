83db76b55e283740659a042ef4c0a3edcd6ec2d8
Brandon Jones <brandonijones@outlook.com>
2022-12-02 20:35:19 -0500

    feat: View Cards Layout Changes
    * Improved carousel with a flip animation for the flashcards.
    * Added a collapsable tags menu for a modern look.
    * Changed the layout of the carousel and viewable flashcards to better fit the tags filter.
    
    Issue #24

--- src/Presentation/Views/ViewFlashcards/ViewFlashcards.tsx
+++ src/Presentation/Views/ViewFlashcards/ViewFlashcards.tsx
@@ -5,6 +5,9 @@ import { onAuthStateChanged } from 'firebase/auth';
 import { auth } from "../../../Data/DataSource/firebase";
 import Carousel from 'react-bootstrap/Carousel';
 import Card from '@mui/material/Card';
+import OffCanvas from 'react-bootstrap/Offcanvas';
+import TuneIcon from '@mui/icons-material/Tune';
+
 
 import './view-flashcards.css';
 
@@ -18,6 +21,9 @@ function ViewFlashcards() {
         currentTagNames,
         checkboxInputs,
         carouselIndex,
+        showFilterSettings,
+        onClickHandleShowFilterSettings,
+        onClickHandleCloseFilterSettings,
         onSelectNextCard,
         onClickToggleSelectTag,
         onClickFilterTags,
@@ -27,6 +33,9 @@ function ViewFlashcards() {
 
     // Carousel Function
     const authFlag = useRef(true); 
+
+    //Flip Function
+    const [flip, setFlip] = useState(false);
 
     useEffect(() => {
         onAuthStateChanged(auth, (currentUser) =>  {
@@ -39,75 +48,86 @@ function ViewFlashcards() {
 
     return (
         <div className='container'>
-            <div className='row border'>
-                <div className='col border'>
-                    {/* Tag Filters */}
-                    {/* TODO make into it's own component */}
-                    <h1>Filters</h1>
-                    <Formik
-                        enableReinitialize={true}
-                        initialValues={initialValues}
-                        validationSchema={validationSchema}
-                        onSubmit={onClickHandleFilters}
-                    >
-                        {({ values }) => (
-                            <Form>
-                                <label>
-                                    <Field type='checkbox' name='showOnlyCurrentUser' onClick={(e: any) => onClickFilterTags(e, values)} />
-                                    <span> Only my cards</span>
-                                </label>
-                                <hr />
+            <div className='row'>
+                {/* Tag Filters */}
+                {/* TODO make into it's own component */}
+                <OffCanvas show={showFilterSettings} onHide={onClickHandleCloseFilterSettings}>
+                    <OffCanvas.Header closeButton>
+                        <OffCanvas.Title>
+                            <h1>Filters</h1>
+                        </OffCanvas.Title>
+                    </OffCanvas.Header>
+                    <OffCanvas.Body>
+                        <Formik
+                            enableReinitialize={true}
+                            initialValues={initialValues}
+                            validationSchema={validationSchema}
+                            onSubmit={onClickHandleFilters}
+                        >
+                            {({ values, resetForm }) => (
+                                <Form className='container'>
+                                    <label>
+                                        <Field type='checkbox' name='showOnlyCurrentUser' onClick={(e: any) => onClickFilterTags(e, values)} />
+                                        <span> Only my cards</span>
+                                    </label>
+                                    <hr />
 
-                                <div id='select-tags-group'>Tags</div>
-                                <div ref={checkboxInputs} role='group' aria-labelledby='select-tags-group'>
-                                    {
-                                        tags.map((tag, index) => {
-                                            return (
-                                                <div key={tag.id}>
-                                                    <label>
-                                                        <Field id={tag.id} type='checkbox' name='selected' value={tag.id} onClick={(e: any) => onClickToggleSelectTag(e, tag)} />
-                                                        <span> { tag.name } </span>
-                                                    </label>
-                                                </div>
-                                                
-                                            );
-                                        })
-                                    }
-                                </div>
-                                
-                                <label>
+                                    <div id='select-tags-group'>Tags</div>
+                                    <div ref={checkboxInputs} role='group' aria-labelledby='select-tags-group'>
+                                        {
+                                            tags.map((tag, index) => {
+                                                return (
+                                                    <div key={tag.id}>
+                                                        <label>
+                                                            <Field id={tag.id} type='checkbox' name='selected' value={tag.id} onClick={(e: any) => onClickToggleSelectTag(e, tag)} />
+                                                            <span> { tag.name } </span>
+                                                        </label>
+                                                    </div>
+                                                    
+                                                );
+                                            })
+                                        }
+                                    </div>
 
-                                </label>
+                                    <div className='text-center mb-3'>
+                                        <button type='submit' className='btn btn-success' onClick={onClickHandleCloseFilterSettings}>Apply Filters</button>
+                                        <ErrorMessage name='selected' render={message => <div className='text-danger'>{message}</div>} />
+                                    </div>
+                                </Form>
+                            )}
+                            
+                        </Formik>
+                    </OffCanvas.Body>
+                </OffCanvas>
+                <div className='col'>
 
-                                <div className='container text-center mb-3'>
-                                    
-                                    <button type='submit' className='btn btn-success'>Apply Filters</button>
-                                    <ErrorMessage name='selected' render={message => <div className='text-danger'>{message}</div>} />
-                                </div>
-                            </Form>
-                        )}
-                        
-                    </Formik>
-
-                </div>
-                <div className='col border'>
-
-                    <div className='row border'>
-                        <div className='col border'>
+                    <div className='row'>
+                        <div className='col'>
                             {/* TODO make into its own component */}
-                            <h1>Categories</h1>
-                            {
-                                currentTagNames.current.map((currentTagName, index) => {
-                                    return (
-                                        <span key={index} className='current-tag'> { currentTagName } </span>
-                                    )
-                                })
-                            }
+                            <div className='mb-2 mt-4'>
+                                <span className='m-2 pt-3 view-cards-title'>Tags</span>
+                                <div className='filter-button'>
+                                    <button  onClick={onClickHandleShowFilterSettings}><TuneIcon /> Filter</button>
+                                </div>
+                            </div>
+                            <div className='container mb-1'>
+                                {
+                                    currentTagNames.current.length > 0 ?
+                                    currentTagNames.current.map((currentTagName, index) => {
+                                        return (
+                                            <span key={index} className='current-tag'> { currentTagName } </span>
+                                        )
+                                    }) :
+                                    <span className='current-tag'>All tags</span>
+                                }
+                                <hr />
+                            </div>
+                            
                         </div>
                     </div>
 
-                    <div className='row border'>
-                        <div className='col border'>
+                    <div className='row'>
+                        <div className='col'>
                             {/* TODO make into its own componenet */}
                             {/* Flashcard carousel */}
                             <Carousel indicators={false} interval={null} activeIndex={carouselIndex} onSelect={onSelectNextCard}>
@@ -116,12 +136,16 @@ function ViewFlashcards() {
                                     flashcards.map((flashcard, index) => {
                                         return (
                                             <Carousel.Item>
-                                                <Card className='view-cards flashcard-carousel'>
-                                                    <div className='flashcard-carousel-text'> 
-                                                        <p>{ flashcard.question }</p>
-                                                        {/* <h3>{ flashcard.answer }</h3> */}
+                                                <div className={`flashcard view-cards ${flip ? 'flip' : ''}`} onClick={() => setFlip(!flip)}>
+                                                    <div className="front">
+                                                        <p className='text-center'>{flashcard.question}</p>
                                                     </div>
-                                                </Card>
+                                                    <div className="back">
+                                                        <p className='text-center'>{flashcard.answer}</p>
+                                                    </div>
+                                                    {/* <p>{flip ? flashcard.answer : flashcard.question}</p> */}
+                                                    {/* <h3>{ flashcard.answer }</h3> */}
+                                                </div>
                                             </Carousel.Item>
                                             
                                         );
@@ -131,26 +155,27 @@ function ViewFlashcards() {
                         </div>
                     </div>
 
+                    <hr />
+
                     {/* Array mapping of all the flashcards */}
-                    <div className='row border'>
-                        <div className='col border'>
+                    <div className='row'>
+                        <div className='col'>
                             {/* Flashcard */}
                             {/* TODO make into its own component */}
-                            <h1>Start of flashcards</h1>
+                            <p className='m-2 view-cards-title'>Cards in this set</p>
                             {
                                 flashcards.map((flashcard, index) => {
                                     return (
-                                        <Card className='m-2 row view-cards' key={flashcard.id}>
+                                        <div className='m-2 row view-cards' key={flashcard.id}>
                                             <p className='p-3 col-5 question-section'>{ flashcard.question }</p>
                                             <p className='p-3 col-7 answer-section'>{ flashcard.answer }</p>
-                                        </Card>
+                                        </div>
                                     );
                                 })
                             }
                         </div>
                     </div>
                 </div>
-
             </div>
         </div>
     );
